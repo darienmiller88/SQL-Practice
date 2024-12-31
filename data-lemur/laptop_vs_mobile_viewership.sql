0686ee43d1c55691845c07d2489116059d1314bc
darienmiller88 <32966645+darienmiller88@users.noreply.github.com>
2024-12-31 00:00:08 -0500

    Update laptop_vs_mobile_viewership.sql

--- data-lemur/laptop_vs_mobile_viewership.sql
+++ data-lemur/laptop_vs_mobile_viewership.sql
@@ -4,6 +4,8 @@ Assume you're given the table on user viewership categorised by device type wher
 Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet 
 and phone viewership. Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices 
 as mobile_views.
+
+In order to do this, I simply counted how many devices are laptops, and how many are either a phone or tablet.
 */
 
 SELECT COUNT(
